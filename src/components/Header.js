4404095186c84b2766875432b0889587eceaa117
Tomasz Skupien <Szybko872@gmail.com>
2022-05-31 12:51:17 +0200

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { SearchOutlined, ShoppingBasket } from '@material-ui/icons'
-
+import '../styles/Header.scss'
 function Header() {
   return (
     <div className='header'>
         <nav>
-            <ul className="ui-items">
+            <ul className="ul-items">
                 <li className="li-items">
                     <Link to='/women'>Women</Link>
                     <Link to='/women'>Men</Link>
                     <Link to='/women'>Denim</Link>
-                    <Link to='/women'>The Gift Shop</Link>
+                    <Link to='/women' className='orange'>The Gift Shop</Link>
                     <Link to='/women'>About</Link>
                 </li>
             </ul>
@@ -22,7 +22,19 @@ function Header() {
             <div className="right">
                 <div className="search">
                     <SearchOutlined className="search-icon"/>
-                    <button>Search</button>
+                    <button className='sch-btn'>Search</button>
+                </div>
+
+                <div className="right-buttons">
+                    <button>
+                        <Link to='/login'>Log In</Link>
+                    </button>
+
+                    <button>
+                        <Link to='/sign-up'>Sign Up</Link>
+                    </button>
+
+                    <ShoppingBasket className='basket'/>
                 </div>
             </div>
         </nav>
---
+++ src/components/Section.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Section.scss';
+
+function Section() {
+  return (
+    <section className="main-section">
+        
+    </section>
+  )
+}
+
+export default Section
