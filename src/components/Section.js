2a167684a843f81ac9844590eaf91dbb612f3e19
Tomasz Skupien <Szybko872@gmail.com>
2022-05-31 13:58:59 +0200

    push

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,14 @@ import '../styles/Section.scss';
 function Section() {
   return (
     <section className="main-section">
-        
+        <div className='content'>
+            <h1>Be Warm, Merry and Stand out</h1>
+            <button>Discover</button>
+        </div>
+
+        <div className="bottom-text">
+            <p>10% of for all productss</p>
+        </div>
     </section>
   )
 }
